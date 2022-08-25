0c57192c73d803d13104a9125ee7c015763628f8
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2022-08-25 15:45:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,8 @@
-import os
-
-from flask import Flask, render_template, request, redirect, send_file, url_for
-
-from s3_demo import list_files, download_file, upload_file
-
+from flask import Flask
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "uploads"
-BUCKET = "insert_bucket_name_here"
 
 
 @app.route('/')
-def entry_point():
-    return 'welcome to myapp.com'
-
-
-@app.route("/storage")
-def storage():
-    contents = list_files(BUCKET)
-    return render_template('storage.html', contents=contents)
-
-
-@app.route("/upload", methods=['POST'])
-def upload():
-    if request.method == "POST":
-        f = request.files['file']
-        f.save(f.filename)
-        upload_file(f"{f.filename}", BUCKET)
-
-        return redirect("/storage")
-
-
-@app.route("/download/<filename>", methods=['GET'])
-def download(filename):
-    if request.method == 'GET':
-        output = download_file(filename, BUCKET)
-
-        return send_file(output, as_attachment=True)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello():
+    return '<h1>Hello, World!</h1>'
